29251c6005cc494fa33a5707a58354a432ebc671
TheRealPear <20259871+TheRealPear@users.noreply.github.com>
2023-07-15 23:00:46 -0400

    Make ImageSwitcher a little better
    Signed-off-by: TheRealPear <20259871+TheRealPear@users.noreply.github.com>

--- src/components/ImageSwitcher.js
+++ src/components/ImageSwitcher.js
@@ -1,15 +1,27 @@
 import React from "react";
 import {useColorMode} from "@docusaurus/theme-common";
 
-const ImageSwitcher = ({ lightImageSrc, darkImageSrc }) => {
+const ImageSwitcher = ({ lightImageSrc, darkImageSrc, alt, loading }) => {
   const { colorMode, setColorMode } = useColorMode();
 
   return (
-    <img
-      src={colorMode === 'dark' ? darkImageSrc : lightImageSrc}
-      alt="This image needs JS enabled to load."
-    />
+    <>
+      <img
+        src={colorMode === 'dark' ? darkImageSrc : lightImageSrc}
+        alt={alt}
+        title={alt}
+        loading={loading}
+      />
+      <noscript>
+        <p>This image requires JavaScript to be enabled.</p>
+      </noscript>
+    </>
   );
+};
+
+ImageSwitcher.defaultProps = {
+  alt: "",
+  loading: "eager",
 };
 
 export default ImageSwitcher;
